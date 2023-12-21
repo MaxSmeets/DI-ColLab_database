32b131e9651646f4539c3606f11d30d6ea25ae5f
Sander Lienaerts <sander.lienaerts@codeforward.nl>
2023-12-21 15:22:11 +0100

    Create user and session models

---
+++ models/session.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+from sqlalchemy import Integer, DateTime, String
+from sqlalchemy.orm import mapped_column, Mapped
+
+from db import Base
+from models.user import TABLE_NAME as USER_TABLE_NAME, User
+
+TABLE_NAME = "session"
+
+
+class Session(Base):
+    """
+    Session model
+
+    A session is created when a user logs in and monitors the user's
+    last known activity within the Collab environment.
+    """
+
+    __tablename__ = TABLE_NAME
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey=f"{USER_TABLE_NAME}.id")
+    creation_date: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow)
+    last_activity: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow)
+    last_known_location: Mapped[str] = mapped_column(String(80))
+
+    user: Mapped[User] = mapped_column(User)
+
+    def __repr__(self):
+        return f"{self.user.username} - {self.creation_date} - {self.last_activity}"
---
+++ models/user.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import mapped_column, Mapped
+from db import Base
+
+TABLE_NAME = "user"
+
+
+class User(Base):
+    """
+    User model
+
+    A user is a person who has access to the Collab environment.
+    """
+
+    __tablename__ = TABLE_NAME
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    username: Mapped[str] = mapped_column(String(80), unique=False, nullable=False)
+    email: Mapped[str] = mapped_column(String)
+
+    def __repr__(self):
+        return f"{self.username} - {self.email}"
